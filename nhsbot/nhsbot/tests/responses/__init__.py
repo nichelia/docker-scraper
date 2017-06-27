47a218e101e0159b29e82c76592d3575089e1e62
nichelia <nich.elia@gmail.com>
2017-06-27 19:26:18 +0100

    Added sample testing file.

---
+++ nhsbot/nhsbot/tests/responses/__init__.py
@@ -0,0 +1,29 @@
+import os
+
+from scrapy.http import TextResponse, Request
+
+def fake_response_from_file(file_name, url=None):
+    """
+    Create a Scrapy fake HTTP response from a HTML file
+    @param file_name: The relative filename from the responses directory,
+                      but absolute paths are also accepted.
+    @param url: The URL of the response.
+    returns: A scrapy HTTP response which can be used for unittesting.
+    """
+    if not url:
+        url = 'http://www.example.com'
+
+    request = Request(url=url)
+    if not file_name[0] == '/':
+        responses_dir = os.path.dirname(os.path.realpath(__file__))
+        file_path = os.path.join(responses_dir, file_name)
+    else:
+        file_path = file_name
+
+    file_content = open(file_path, 'r').read()
+
+    response = TextResponse(url=url,
+        request=request,
+        body=file_content)
+    
+    return response
